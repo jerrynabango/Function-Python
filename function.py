896d61a6a94de62c46febd09a9f4681399d53111
Jerry Nabango <jerrynabango9@gmail.com>
2024-03-04 11:53:22 +0300

    simple function

--- function.py
+++ function.py
@@ -5,12 +5,13 @@ def apply_discount(original_price, discount_percent):
     else:
         return original_price
 
+
 initial_price = float(input("Please input the initial price of the item: "))
 discount_rate = float(input("Please enter the discount rate: "))
 
 final_price = apply_discount(initial_price, discount_rate)
 
 if final_price != initial_price:
-    print(f"The discounted final price is: ${final_price:.2f}")
+    print(f"The discounted final price is: ksh {final_price:.2f}")
 else:
-    print("No discount applied. The original price remains: ksh{:.2f}".format(initial_price))
+    print(f"No discount applied. The original price remains: ksh {initial_price:.2f}")
